1a7c827ac37bed9ac0cae67c200c71af1a00a947
Marc-Antoine Fournier <mafournier000@hotmail.com>
2020-02-17 21:05:28 -0500

    Ajout de la page de tableau de bord. Elle est non terminée.

--- ProjetMaintenancePython/machines/urls.py
+++ ProjetMaintenancePython/machines/urls.py
@@ -3,5 +3,6 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.header, name='header')
+    path('header', views.header, name='header'),
+    path('', views.tableaubord, name='tableaubord')
 ]
--- ProjetMaintenancePython/machines/views.py
+++ ProjetMaintenancePython/machines/views.py
@@ -7,3 +7,7 @@ from django.template import loader
 def header(request):
     template = loader.get_template('machines/header.html')
     return HttpResponse(template.render({}, request))
+
+def tableaubord(request):
+    template = loader.get_template('machines/tableaubord.html')
+    return HttpResponse(template.render({}, request))
