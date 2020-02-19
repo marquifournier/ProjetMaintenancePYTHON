cf364e7642bd1f970ec2081543ea32efb2b079ec
Marc-Antoine Fournier <1757006@cstjean.qc.ca>
2020-02-19 13:24:12 -0500

    Correction du bugfix

--- ProjetMaintenancePython/machines/views.py
+++ ProjetMaintenancePython/machines/views.py
@@ -1,20 +1,23 @@
-from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
 
+
 def header(request):
     template = loader.get_template('machines/header.html')
     return HttpResponse(template.render({}, request))
+
 
 def tableaubord(request):
     template = loader.get_template('machines/interfaceorganisation.html')
     return HttpResponse(template.render({}, request))
 
+
 def gererEssais(request):
     template = loader.get_template('machines/gererEssais.html')
     return HttpResponse(template.render({}, request))
+
 
 def essai(request):
     template = loader.get_template('machines/essai.html')
